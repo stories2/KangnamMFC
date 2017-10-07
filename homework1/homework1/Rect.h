70788c3d82b24b7075f151122198f7292550e4a0
Kim HyunWoo <stories2@naver.com>
2017-10-08 01:42:42 +0900

    homework module : rect class coded

--- homework1/homework1/Rect.h
+++ homework1/homework1/Rect.h
@@ -3,5 +3,17 @@
 using namespace std;
 
 class Rect : public Shape {
-
+	float width, height;
+public:
+	Rect(double x, double y, double w, double h);
+	void Draw();
 };
+
+Rect::Rect(double x, double y, double w, double h) :Shape(x, y) {
+	width = w;
+	height = h;
+}
+
+void Rect::Draw() {
+	cout << "[RECTANGLE] position = (" << x << ", " << y << ")" << ", size = (" << width << ", " << height << ")" << endl;
+}
